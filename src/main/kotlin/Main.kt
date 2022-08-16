4c71d8fe3211cf88e1ed8f27d2404f67c67cddb5
itshash01 <110396370+itshash01@users.noreply.github.com>
2022-08-16 17:10:27 +0400

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class RockPaperScissors {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun play(playerOneInput: String, playerTwoInput: String): String? {
+        val (winner, loser) = getWinnerLoserCombination(playerOneInput, playerTwoInput)
+        return winnerMessage(winner, loser)
+    }
+
+    private fun isATie(winner: String, loser: String): Boolean {
+        return winner == loser
+    }
+
+    private fun getWinnerLoserCombination(playerOneInput: String, playerTwoInput: String): Pair<String, String> {
+        return if (isPlayerOneWInner(playerOneInput, playerTwoInput))
+            Pair(playerOneInput, playerTwoInput)
+        else
+            Pair(playerTwoInput, playerOneInput)
+    }
+
+    private fun isPlayerOneWInner(playerOneInput: String, playerTwoInput: String): Boolean {
+        return (playerOneInput == "rock" && playerTwoInput == "scissors") ||
+                (playerOneInput == "paper" && playerTwoInput == "rock") ||
+                (playerOneInput == "scissors" && playerTwoInput == "paper")
+    }
+
+    private fun winnerMessage(winner: String, loser: String) =
+            if (isATie(winner, loser))
+                "tie"
+            else getWinningDisplayMessage(winner, loser)
+
+    private fun getWinningDisplayMessage(winner: String, loser: String) = "$winner beats $loser"
 }
